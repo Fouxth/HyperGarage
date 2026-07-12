92e5dc2236bf038ec235d1eddd0c2822c4d028e3
Fouxth <fourthptrps@gmail.com>
2026-07-13 06:34:09 +0700

    Implement SPA redirect hack for clean URLs and fix direct access 404s on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './i18n'
 import './index.css'
@@ -15,9 +15,9 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <CartProvider>
         <WishlistProvider>
-          <HashRouter>
+          <BrowserRouter>
             <App />
-          </HashRouter>
+          </BrowserRouter>
         </WishlistProvider>
       </CartProvider>
     </QueryClientProvider>
